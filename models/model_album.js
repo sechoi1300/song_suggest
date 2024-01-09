ddd22b1d0f9d8cc68edcdec00637dc07edfd00db
Eric Choi <sechoi1300@gmail.com>
2024-01-08 22:17:41 -0800

    Finished album model
    Filled in appropriated schema info for album model.

---
+++ models/model_album.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const AlbumSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    artist: {
+        type: String,
+        required: true
+    },
+    released_date: {
+        type: Date,
+        required: true
+    },
+    length: {
+        type: Number,
+        required: true
+    },
+    num_of_songs: {
+        type: Number,
+        required: true
+    },
+    genres: {
+        type: Array,
+    },
+    updated_date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = Album = mongoose.model('album', AlbumSchema);
