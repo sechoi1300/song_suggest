c60bd820ac2545b74010560b33eb9352d360f6e5
Eric Choi <sechoi1300@gmail.com>
2024-01-09 16:59:15 -0800

    Added routes and component render
    Defined all the routes and rendered the corresponding component for each path.

--- frontend/song_suggest/src/main.jsx
+++ frontend/song_suggest/src/main.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import './index.css'
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 
+import CreateAlbum from "./components/CreateAlbum";
+import ShowAlbumList from "./components/ShowAlbumList";
+import ShowAlbumDetails from "./components/ShowAlbumDetails";
+import UpdateAlbumInfo from "./components/UpdateAlbumInfo";
+
+const router = createBrowserRouter([
+  { path: "/", element: <ShowAlbumList /> },
+  { path: "/create-book", element: <CreateAlbum /> },
+  { path: "/show-book/:id", element: <ShowAlbumDetails /> },
+  { path: "/edit-book/:id", element: <UpdateAlbumInfo /> },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
