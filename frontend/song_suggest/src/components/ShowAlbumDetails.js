6e607de20c7f45e2458d57e1376e0a9e348b4f66
Eric Choi <sechoi1300@gmail.com>
2024-01-11 14:57:30 -0800

    Finished ShowAlbumDetails
    Finished ShowAlbumDetails.js.

--- frontend/song_suggest/src/components/ShowAlbumDetails.js
+++ frontend/song_suggest/src/components/ShowAlbumDetails.js
@@ -11,105 +11,115 @@ function ShowAlbumDetails(props) {
 
     useEffect(() => {
         axios
-        .get(`http://localhost:8082/api/albums/${id}`)
-        .then((res) => {
-            setAlbum(res.data);
-        })
-        .catch((err) => {
-            console.log('Error from ShowAlbumDetails');
-        });
+            .get(`http://localhost:8082/api/albums/${id}`)
+            .then((res) => {
+                setAlbum(res.data);
+            })
+            .catch((err) => {
+                console.log('Error from ShowAlbumDetails');
+            });
     }, [id]);
 
     const onDeleteClick = (id) => {
         axios
-        .delete(`http://localhost:8082/api/albums/${id}`)
-        .then((res) => {
-            navigate('/');
-        })
-        .catch((err) => {
-            console.log('Error form ShowAlbumDetails_deleteClick');
-        });
+            .delete(`http://localhost:8082/api/albums/${id}`)
+            .then((res) => {
+                navigate('/');
+            })
+            .catch((err) => {
+                console.log('Error form ShowAlbumDetails_deleteClick');
+            });
     };
 
     const AlbumItem = (
         <div>
-        <table className='table table-hover table-dark'>
-            <tbody>
-            <tr>
-                <th scope='row'>1</th>
-                <td>Title</td>
-                <td>{album.title}</td>
-            </tr>
-            <tr>
-                <th scope='row'>2</th>
-                <td>artist</td>
-                <td>{book.author}</td>
-            </tr>
-            <tr>
-                <th scope='row'>3</th>
-                <td>ISBN</td>
-                <td>{book.isbn}</td>
-            </tr>
-            <tr>
-                <th scope='row'>4</th>
-                <td>Publisher</td>
-                <td>{book.publisher}</td>
-            </tr>
-            <tr>
-                <th scope='row'>5</th>
-                <td>Published Date</td>
-                <td>{book.published_date}</td>
-            </tr>
-            <tr>
-                <th scope='row'>6</th>
-                <td>Description</td>
-                <td>{book.description}</td>
-            </tr>
-            </tbody>
-        </table>
+            <table className='table table-hover table-dark'>
+                <tbody>
+                    <tr>
+                        <th scope='row'>1</th>
+                        <td>Title</td>
+                        <td>{album.title}</td>
+                    </tr>
+                    <tr>
+                        <th scope='row'>2</th>
+                        <td>artist</td>
+                        <td>
+                            {Array.isArray(album.artist)
+                                ? album.artist.join(', ')
+                                : album.artist
+                            }
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope='row'>3</th>
+                        <td>Release Date</td>
+                        <td>{album.released_date}</td>
+                    </tr>
+                    <tr>
+                        <th scope='row'>4</th>
+                        <td>Length</td>
+                        <td>{album.length}</td>
+                    </tr>
+                    <tr>
+                        <th scope='row'>5</th>
+                        <td>Number of Songs</td>
+                        <td>{album.num_of_songs}</td>
+                    </tr>
+                    <tr>
+                        <th scope='row'>6</th>
+                        <td>Genres</td>
+                        <td>
+                            {Array.isArray(album.genres)
+                                ? album.genres.join(', ')
+                                : album.genres
+                            }
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     );
 
     return (
-        <div className='ShowBookDetails'>
-        <div className='container'>
-            <div className='row'>
-            <div className='col-md-10 m-auto'>
-                <br /> <br />
-                <Link to='/' className='btn btn-outline-warning float-left'>
-                Show Book List
-                </Link>
+        <div className='ShowAlbumDetails'>
+            <div className='container'>
+                <div className='row'>
+                    <div className='col-md-10 m-auto'>
+                        <br /> <br />
+                        <Link to='/' className='btn btn-outline-warning float-left'>
+                            Show Album List
+                        </Link>
+                    </div>
+                    <br />
+                    <div className='col-md-8 m-auto'>
+                        <h1 className='display-4 text-center'>Album Record</h1>
+                        <p className='lead text-center'>View Album Info</p>
+                        <hr /> <br />
+                    </div>
+                    <div className='col-md-10 m-auto'>{AlbumItem}</div>
+                    <div className='col-md-6 m-auto'>
+                        <button
+                            type='button'
+                            className='btn btn-outline-danger btn-lg btn-block'
+                            onClick={() => {
+                                onDeleteClick(album._id);
+                            }}
+                        >
+                            Delete Album
+                        </button>
+                    </div>
+                    <div className='col-md-6 m-auto'>
+                        <Link
+                            to={`/edit-album/${album._id}`}
+                            className='btn btn-outline-info btn-lg btn-block'
+                        >
+                            Edit Album
+                        </Link>
+                    </div>
+                </div>
             </div>
-            <br />
-            <div className='col-md-8 m-auto'>
-                <h1 className='display-4 text-center'>Book's Record</h1>
-                <p className='lead text-center'>View Book's Info</p>
-                <hr /> <br />
-            </div>
-            <div className='col-md-10 m-auto'>{BookItem}</div>
-            <div className='col-md-6 m-auto'>
-                <button
-                type='button'
-                className='btn btn-outline-danger btn-lg btn-block'
-                onClick={() => {
-                    onDeleteClick(book._id);
-                }}
-                >
-                Delete Book
-                </button>
-            </div>
-            <div className='col-md-6 m-auto'>
-                <Link
-                to={`/edit-book/${book._id}`}
-                className='btn btn-outline-info btn-lg btn-block'
-                >
-                Edit Book
-                </Link>
-            </div>
-            </div>
-        </div>
         </div>
     );
 }
 
-export default ShowBookDetails;
+export default ShowAlbumDetails;
